379aa1437bd3fbc41a8f24b24e061316dce89a43
liumapp <liumapp.com@gmail.com>
2018-05-09 17:26:05 +0800

    only use pic-converter-queue

--- conversion-service/convert-pdf-to-img/src/main/java/com/liumapp/convert/img/customer/PicConverterQueueHandler.java
+++ conversion-service/convert-pdf-to-img/src/main/java/com/liumapp/convert/img/customer/PicConverterQueueHandler.java
@@ -1,23 +1,19 @@
 package com.liumapp.convert.img.customer;
 
-import org.springframework.amqp.rabbit.annotation.RabbitHandler;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Component;
 
 /**
  * @author liumapp
- * @file FirstPicConverterHandler.java
+ * @file PicConverterQueueHandler.java
  * @email liumapp.com@gmail.com
  * @homepage http://www.liumapp.com
  * @date 5/9/18
  */
 @Component
-@RabbitListener(queues = "first-pic-converter-queue")
-public class FirstPicConverterHandler {
+@RabbitListener(queues = "pic-converter-queue")
+public class PicConverterQueueHandler {
 
-    @RabbitHandler
-    public void process (String msg) {
-
-    }
+    
 
 }
